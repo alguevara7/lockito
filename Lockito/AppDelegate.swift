6ed0831447935701450f100b409d270593373050
Alexei Guevara <alexguev@DellaLu.local>
2014-06-12 22:28:19 -0400

    Initial Commit

---
+++ Lockito/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lockito
+//
+//  Created by Alexei Guevara on 2014-06-12.
+//  Copyright (c) 2014 Alexei Guevara. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Lockito/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Lockito
+//
+//  Created by Alexei Guevara on 2014-06-12.
+//  Copyright (c) 2014 Alexei Guevara. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
