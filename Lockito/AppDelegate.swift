2cfe78e341bc12dc37e5c785d976bcbbe520db0b
Alexei Guevara <alexguev@DellaLu.local>
2014-06-12 23:50:09 -0400

    basic device detection working

--- Lockito/AppDelegate.swift
+++ Lockito/AppDelegate.swift
@@ -7,20 +7,53 @@
 //
 
 import Cocoa
+import IOBluetooth
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, CBCentralManagerDelegate {
                             
     @IBOutlet var window: NSWindow
-
+    
+    var c: CBCentralManager?
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+        c = CBCentralManager(delegate: self, queue: nil, options: nil);
     }
 
+    @IBAction func start(sender : AnyObject) {
+        c!.scanForPeripheralsWithServices(nil, options: nil);
+    }
+    
+    @IBAction func stop(sender : AnyObject) {
+        c!.stopScan()
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
+        c!.stopScan()
     }
-
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        NSLog("HERE!")
+    }
+    
+    func centralManager(
+        central: CBCentralManager!,
+        didDiscoverPeripheral peripheral: CBPeripheral!,
+        advertisementData: NSDictionary!,
+        RSSI: NSNumber!) {
+     
+            //closest = -62
+            
+            //active = -81
+            
+            //NSLog("\(peripheral.description) -> \(peripheral.identifier.description)")
+            
+            if peripheral.identifier.UUIDString() == "280B431C-145E-47F3-B3B0-4D39D50D4E43" {
+                NSLog("FOUND!")
+                NSLog(RSSI.description)
+                c!.stopScan()
+            }
+    }
 
 }
 
